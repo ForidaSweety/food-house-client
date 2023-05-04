1df9e1e1efd4462db22568c7fae31f03b22329a1
ForidaSweety <foridayeasmin2000@gmail.com>
2023-05-04 20:26:50 +0600

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,48 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { useContext } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
+
+
+// import { useLocation, useHistory, useNavigate } from "react-router";
 
 const Login = () => {
  
     return (
-       <div>
-        
-       </div>
+        <Container className='w-25 mx-auto'>
+        <h3>Please Login</h3>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" name='email' placeholder="Enter email" required />
+
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name='password' placeholder="Password" required />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Login
+            </Button>
+
+            <br />
+
+            <Form.Text className="text-secondary">
+               Do not have an Account? <Link to="/register">Register</Link>
+            </Form.Text>
+
+            <Form.Text className="text-success">
+                We will never share your email with anyone else.
+            </Form.Text>
+            <Form.Text className="text-danger">
+                We will never share your email with anyone else.
+            </Form.Text>
+        </Form>
+    </Container>
     );
 };
 
